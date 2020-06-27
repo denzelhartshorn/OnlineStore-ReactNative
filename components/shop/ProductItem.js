9568481343dc29b7385fe2f2919e3385f5d68bdb
Denzel Hartshorn <denzelhartshorn@gmail.com>
2020-06-27 16:14:02 -0700

    styling redone

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -28,3 +28,50 @@ const ProductItem = props => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
+    height: 300,
+    margin: 20
+  },
+  imageContainer: {
+    width: "100%",
+    height: "60%",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: "hidden"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4
+  },
+  price: {
+    fontSize: 14,
+    color: "#888"
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20
+  }
+});
+
+export default ProductItem;
